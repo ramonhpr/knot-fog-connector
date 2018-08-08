4ff738ddb4a611d8a59d25ac043cc5e7e99649f9
João Neto <joao.alexandre@cesar.org.br>
2018-08-08 16:40:07 -0300

    Setup index to subscribe to requested data

--- esm/index.js
+++ esm/index.js
@@ -8,6 +8,7 @@ import DeviceStore from 'infrastructure/DeviceStore';
 // Domain
 import UpdateDevices from 'interactors/UpdateDevices';
 import SubscribeToUpdatedData from 'interactors/SubscribeToUpdatedData';
+import SubscribeToRequestedData from 'interactors/SubscribeToRequestedData';
 import DevicesService from 'services/DevicesService';
 import DataService from 'services/DataService';
 
@@ -27,14 +28,16 @@ async function main() {
 
     const updateDevices = new UpdateDevices(deviceStore, fogConnection, cloudConnector);
     const subscribeToUpdatedData = new SubscribeToUpdatedData(fogConnection, cloudConnector);
+    const subscribeToRequestedData = new SubscribeToRequestedData(fogConnection, cloudConnector);
 
     const devicesService = new DevicesService(updateDevices);
-    const dataService = new DataService(subscribeToUpdatedData);
+    const dataService = new DataService(subscribeToUpdatedData, subscribeToRequestedData);
 
     await cloudConnector.start();
     await fogConnection.start();
 
     await dataService.subscribeToUpdated();
+    await dataService.subscribeToRequested();
 
     setInterval(devicesService.update.bind(devicesService), 3000);
   } catch (err) {
